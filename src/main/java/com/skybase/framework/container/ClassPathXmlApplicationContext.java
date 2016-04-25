21069190edc57b9b0c8d6da54bdbecc3643eb44e
ubuntuvim <1527254027@qq.com>
2016-04-25 18:33:48 +0800

    容器初始化、获取实例

---
+++ src/main/java/com/skybase/framework/container/ClassPathXmlApplicationContext.java
@@ -0,0 +1,22 @@
+package com.skybase.framework.container;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 读取被实例化的adapter配置
+ */
+public class ClassPathXmlApplicationContext {
+	
+	public ClassPathXmlApplicationContext(String[] cfgArr) {
+	}
+
+	private Map<String, Object> container = new HashMap<>();
+
+	public Map<String, Object> initContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
